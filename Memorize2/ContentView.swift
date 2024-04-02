861dab643c9a0192b96b69389c98bd0b9e11a338
nenad100 <nenadstojanov84@gmail.com>
2024-04-02 23:06:58 +0200

    Required tasks for assignment 1

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -8,25 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻","🎃","🕷️","👿","💀","🕸️","🧙‍♀️","🙀","👹","😱","☠️","🍭"]
+    let haloweenTheme = ["👻","🎃","🕷️","👿","💀","🕸️","🧙‍♀️","🙀","👹","😱","☠️","🍭"];
+    let carTheme = ["🚗","🚙","🏎️","🛺","🚕","🛻","🚖","🚔"]
+    let trainTheme = ["🚂","🚃","🚝","🚅","🚆","🚇","🚉"]
+    @State var emojis: Array<String> = []
+    
     @State var cardCount: Int = 4
     
     var body: some View {
         VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
             ScrollView {
                 cards
             }
             Spacer()
             cardCountAdjusters
+            Spacer()
+            themePickers
         }
         .padding()
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
-                    .aspectRatio(2/3, contentMode: .fit)
+            if (emojis.count > 0) {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
             }
         }
         .foregroundColor(.orange)
@@ -58,11 +68,35 @@ struct ContentView: View {
     var cardAdder: some View {
         return cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
+    
+    var themePickers: some View {
+        HStack {
+            themeButton(themeEmojis: carTheme, symbol: "car", btnLabel: "Vehicles")
+            Spacer()
+            themeButton(themeEmojis: haloweenTheme, symbol: "moon", btnLabel: "Halloween")
+            Spacer()
+            themeButton(themeEmojis: trainTheme, symbol: "train.side.front.car",
+            btnLabel: "Trains")
+        }
+    }
+    
+    func themeButton(themeEmojis: Array<String>, symbol: String, btnLabel: String) -> some View {
+       Button( action: {
+           emojis = (themeEmojis + themeEmojis).shuffled();
+       }, label: {
+           VStack {
+               Image(systemName: symbol)
+                   .imageScale(.large)
+                   .font(.largeTitle)
+               Text(btnLabel)
+           }
+       })
+    }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
