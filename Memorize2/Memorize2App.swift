d28ccf98a2ffa151b2b44ae6231815dbf2b5f8c3
nenadstojanov <nenad.stojanov@endava.com>
2024-04-02 10:44:27 +0200

    Initial Commit

---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Nenad Stojanov on 2.4.24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
