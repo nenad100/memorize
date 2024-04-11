e2b56b052dc0447a604a73e9e4b8422df0ddb5f2
nenad100 <nenadstojanov84@gmail.com>
2024-04-11 13:36:14 +0200

    Annimations

---
+++ Memorize2/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Memorize2
+//
+//  Created by Nenad Stojanov on 10.4.24.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
